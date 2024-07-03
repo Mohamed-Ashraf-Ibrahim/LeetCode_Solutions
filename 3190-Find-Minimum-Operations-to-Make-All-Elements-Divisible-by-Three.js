19541cf8f1ca06778cd4531d7ffd297aa1db1011
Mohamed Ashraf <146873948+Mohamed-Ashraf-Ibrahim@users.noreply.github.com>
2024-07-03 23:41:35 +0300

    [3190] [Time Beats: 68.31%] [Memory Beats: 29.41%] - LeetPush

--- 3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.js
+++ 3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.js
@@ -6,7 +6,6 @@ var minimumOperations = function(nums) {
     let operations = 0;
     for (let num of nums){
         let remainder  = num % 3;
-        
         remainder === 1 ? operations += 1 : remainder === 2 ? operations += 1 : operations += 0
     }
     return operations;
